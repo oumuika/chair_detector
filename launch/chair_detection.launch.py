8e69ae6aaaf654e8874001a890df2c52388337dc
oumuika <oumuika@gmail.com>
2024-12-23 17:05:55 +0900

    update

--- launch/chair_detection.launch.py
+++ launch/chair_detection.launch.py
@@ -15,30 +15,30 @@ def generate_launch_description():
         executable='scan_to_pointcloud',  # ノードの実行ファイル名
         name='scan_to_pointcloud',
         output='screen',  # ログ出力をスクリーンに表示
-        parameters=[]
-    ),
+        #parameters=[]
+    )
 
     leg_detecor = Node(
         package='chair_detector',  # leg_detectorが含まれるパッケージ名
         executable='leg_detector',  # ノードの実行ファイル名
         name='leg_detector',
         output='screen',  # ログ出力をスクリーンに表示
-        parameters=[]
-    ),
+        #parameters=[]
+    )
 
     chair_detecor = Node(
         package='chair_detector',  # chair_detectorが含まれるパッケージ名
         executable='chair_detector',  # ノードの実行ファイル名
         name='chair_detector',
         output='screen',  # ログ出力をスクリーンに表示
-        parameters=[]
+        #parameters=[]
     )
 
     laser_filter = Node(
         package   = 'laser_filters',
         executable= 'scan_to_scan_filter_chain',
         parameters=['/home/oumuika/Documents/mirs2403/src/chair_detector/config/range_filter.yaml'],
-    ),
+    )
 
     ld = LaunchDescription()
     ld.add_action(scan_to_pointcloud)
